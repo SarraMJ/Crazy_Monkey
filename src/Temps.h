16f46f514178bf0b30e81a30e33a8156d41576eb
Maya Soufan <maya.soufan@etu.univ-lyon1.fr>
2023-03-07 10:21:16 +0100

    Ajout de la classe Temps

---
+++ src/Temps.h
@@ -0,0 +1,65 @@
+#ifndef _TEMPS_H
+#define _TEMPS_H
+
+/**
+ * @class Temps
+ * @brief Classe temps qui stocke les minutes et les secondes 
+ * 
+ */
+class Temps {
+
+    private: 
+
+    unsigned int min;  
+    unsigned int sec; 
+
+    public : 
+
+    /**
+     * @brief Constructeur par défaut
+     * 
+     */
+    Temps(); 
+
+    /**
+     * @brief Constructeur par copie
+     * 
+     * @param[in] m 
+     * @param[in] s 
+     */
+    Temps(unsigned int m, unsigned int s);
+
+    /**
+     * @brief Accesseur qui récupère les secondes
+
+     * 
+     * @param[out] unsigned int& 
+     */
+    unsigned int  getSec() const;
+
+    /**
+     * @brief Accesseur qui récupère les minutes 
+     * 
+     * @param[out] unsigned int& 
+     */
+    unsigned int getMin() const ; // on a enlevé le & car ça marchait pas sans, à revoir si il faut le mettre dans les deux get
+
+    /**
+     * @brief Mutateur qui modifie la composante seconde
+     * 
+     * @param[in] s 
+     */
+    void setSec(unsigned int s);
+
+    /**
+     * @brief Mutateur qui modifie la composante minute
+     * 
+     * @param[in] m 
+     */
+    void setMin(unsigned int m);
+
+
+
+};
+
+#endif
